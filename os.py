310b347c6fa14de623df3b91362308bdfdb72c04
ap185428 <78581835+ap185428@users.noreply.github.com>
2021-02-19 15:50:21 +0530

    Create os.py

---
+++ os.py
@@ -0,0 +1,22 @@
+# importing module 
+import os 
+  
+# path of the current script 
+path = '/public'
+  
+# Before creating 
+dir_list = os.listdir(path)  
+print("List of directories and files before creation:") 
+print(dir_list) 
+print() 
+  
+# Creates a new file 
+with open('myfile.txt', 'w') as fp: 
+    pass
+    # To write data to new file uncomment 
+    # this fp.write("New file created") 
+  
+# After creating  
+dir_list = os.listdir(path) 
+print("List of directories and files after creation:") 
+print(dir_list) 
